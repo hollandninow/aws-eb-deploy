127387ee6782e3de09d868890c003d1bcf034a24
Holland Ninow <holland.ninow@gmail.com>
2023-03-14 08:19:40 -0700

    Step by step description of deploying app to AWS EB

---
+++ cdk-eb-infra-stack.js
@@ -0,0 +1,99 @@
+const cdk = require("@aws-cdk/core");
+const s3assets = require("@aws-cdk/aws-s3-assets");
+const elasticbeanstalk = require("@aws-cdk/aws-elasticbeanstalk");
+const iam = require("@aws-cdk/aws-iam");
+
+class CdkEbInfraStack extends cdk.Stack {
+  /**
+   *
+   * @param {cdk.Construct} scope
+   * @param {string} id
+   * @param {cdk.StackProps=} props
+   */
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // Construct an s3 asset from the zip located from directory up.
+    const webAppZipArchive = new s3assets.Asset(this, "WebAppZip", {
+      path: `${__dirname}/../app.zip`,
+    });
+
+    // Create an Elastic Beanstalk app
+    const appName = "%APP_NAME%";
+    const app = new elasticbeanstalk.CfnApplication(this, "Application", {
+      applicationName: appName,
+    });
+
+    // Create an app version from the s3 asset defined earlier
+    const appVersionProps = new elasticbeanstalk.CfnApplicationVersion(
+      this,
+      "AppVersion",
+      {
+        applicationName: appName,
+        sourceBundle: {
+          s3Bucket: webAppZipArchive.s3BucketName,
+          s3Key: webAppZipArchive.s3ObjectKey,
+        },
+      }
+    );
+
+    // Make sure that Elastic Beanstalk app exists before creating an app version
+    appVersionProps.addDependsOn(app);
+
+    // Create role and instance profile
+    const myRole = new iam.Role(
+      this,
+      `${appName}-aws-elasticbeanstalk-ec2-role`,
+      {
+        assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
+      }
+    );
+
+    const managedPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName(
+      "AWSElasticBeanstalkWebTier"
+    );
+    myRole.addManagedPolicy(managedPolicy);
+
+    const myProfileName = `${appName}-InstanceProfile`;
+
+    const instanceProfile = new iam.CfnInstanceProfile(this, myProfileName, {
+      instanceProfileName: myProfileName,
+      roles: [myRole.roleName],
+    });
+
+    // Example of some options which can be configured
+    const optionSettingProperties = [
+      {
+        namespace: "aws:autoscaling:launchconfiguration",
+        optionName: "IamInstanceProfile",
+        value: myProfileName,
+      },
+      {
+        namespace: "aws:autoscaling:asg",
+        optionName: "MinSize",
+        value: "1",
+      },
+      {
+        namespace: "aws:autoscaling:asg",
+        optionName: "MaxSize",
+        value: "1",
+      },
+      {
+        namespace: "aws:ec2:instances",
+        optionName: "InstanceTypes",
+        value: "t2.micro",
+      },
+    ];
+
+    // Create an Elastic Beanstalk environment to run the application
+    const elbEnv = new elasticbeanstalk.CfnEnvironment(this, "Environment", {
+      environmentName: "%APPNAME%Environment",
+      applicationName: app.applicationName || appName,
+      solutionStackName: "64bit Amazon Linux 2 v5.6.4 running Node.js 14",
+      optionSettings: optionSettingProperties,
+      versionLabel: appVersionProps.ref,
+    });
+  }
+}
+
+module.exports = { CdkEbInfraStack };
---
+++ main.js
@@ -0,0 +1,51 @@
+/*
+
+***REQUIREMENTS***
+
+- AWS CLI is configured
+- User has AWS account
+- User has bootstrapped AWS account in AWS CLI
+- User has created a non-containerized web app using Node JS and express
+
+***PROCESS***
+
+0. Set up. Request the following:
+
+  0a. APP_NAME
+
+  0b. App's root folder path
+
+  0c. CDK version to use (provide options)
+
+  0d. AWS_ACCOUNT_NUMBER
+
+  0e. AWS_REGION
+
+1. Create the CDK app
+
+  1a. Run the following:
+    i.    npm install -g cdk@1.180.0
+    ii.   npm i —save-exact @aws-cdk/aws-s3-assets@1.180.0
+    iii.  npm i —save-exact @aws-cdk/aws-elasticbeanstalk@1.180.0
+    iv.   npm i —save-exact @aws-cdk/aws-iam@1.180.0
+
+  1b. Create new directory "cdk-eb-infra" in root directory of app
+
+  1c. Move to directory "cdk-eb-infra"
+
+  1d. Initialize CDK application using "cdk init app --language javascript"
+
+  1e. In /lib/cdk-eb-infra-stack.js, replace existing code and paste code from ./cdk-eb-infra-stack.js and substitute %APP_NAME% for APP_NAME requested in 1a.
+
+  1f. Run the following while in directory "cdk-eb-infra"
+    i.    npm i @aws-cdk/aws-s3-assets
+    ii.   npm i @aws-cdk/aws-elasticbeanstalk
+    iii.  npm i @aws-cdk/aws-iam
+
+
+2. Zip the Node.js app as "app.zip" into "cdk-eb-infra" folder.
+
+3. Bootstrap account using AWS_ACCOUNT_NUMBER and AWS_REGION in the command "cdk bootstrap aws://AWS_ACCOUNT_NUMBER/AWS_REGION"
+
+4. Run "cdk deploy"
+*/
